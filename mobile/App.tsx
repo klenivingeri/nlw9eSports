b13d6c30507e5f58102408c6c891119081e40667
Erick Kleniving <libertti@live.com>
2022-09-14 20:23:01 -0300

    mobile - inserindo logo

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -24,7 +24,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
-      { fontsLoaded ? <Home /> :  <Loading /> }
+      { fontsLoaded ? <Home /> : <Loading /> }
     </Background>
     )
 }
