f1994d67148693b4fdf84178c1cbef7d34db9c34
Erick Kleniving <libertti@live.com>
2022-09-14 18:36:53 -0300

    Descrição dos jogos com bordas arredondadas

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,6 @@
 import './styles/main.css'
 import logoImg from '/logo-nlw.svg'
 
-
 function App() {
   return (
   <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
@@ -13,24 +12,48 @@ function App() {
       </span>
       está aqui.
     </h1>
-    <div className="grid grid-cols-6 gap-6">
-      <a href="">
+    <div className="grid grid-cols-6 gap-6 mt-16">
+      <a href="" className="relative rounded-lg overflow-hidden">
         <img src="/game1.png" alt="" />
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 ">
+          <strong className="font-bold text-white block">League of legends</strong>
+          <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+      <a href="" className="relative rounded-lg overflow-hidden">
         <img src="/game2.png" alt="" />
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 ">
+          <strong className="font-bold text-white block">Dota 2</strong>
+          <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+      <a href="" className="relative rounded-lg overflow-hidden">
         <img src="/game3.png" alt="" />
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 ">
+          <strong className="font-bold text-white block">Counter Strike</strong>
+          <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+      <a href="" className="relative rounded-lg overflow-hidden">
         <img src="/game4.png" alt="" />
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 ">
+          <strong className="font-bold text-white block">Apex Legends</strong>
+          <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+      <a href="" className="relative rounded-lg overflow-hidden">
         <img src="/game5.png" alt="" />
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 ">
+          <strong className="font-bold text-white block">Fortnite</strong>
+          <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+        </div>
       </a>
-      <a href="">
+      <a href="" className="relative rounded-lg overflow-hidden">
         <img src="/game6.png" alt="" />
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 ">
+          <strong className="font-bold text-white block">World of Warcraft</strong>
+          <span className="text-zinc-300 text-sm block mt-1">4 anúncios</span>
+        </div>
       </a>
 
 
