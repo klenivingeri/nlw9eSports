2eade6f37e53a7342036011342b4249bb7100a55
Erick Kleniving <libertti@live.com>
2022-09-14 18:12:19 -0300

    Clip text transparent

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,20 @@
 import './styles/main.css'
+import logoImg from '../src/assets/logo-nlw.svg'
+
+
 function App() {
-  return <div className="w-8 h-8 bg-violet-500">Hellow World</div>
+  return (
+  <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <img src={logoImg}  alt="" />
+    <h1 className="text-6xl text-white font-black my-20">
+      Seu
+      <span className="bg-nlw-gradient text-transparent bg-clip-text">
+        duo
+      </span>
+      está aqui.
+    </h1>
+  </div>
+  )
 }
 
 export default App
